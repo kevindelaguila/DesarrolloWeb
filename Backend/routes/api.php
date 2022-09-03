dcc91da4325c8944ad47913c1bd9c6a7f72e72d9
kevindelaguila <kevindelaguila007@gmail.com>
2022-09-02 19:54:19 -0600

    JWT config

--- Backend/routes/api.php
+++ Backend/routes/api.php
@@ -1,9 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
-
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,13 +10,16 @@ use App\Http\Controllers\UserController;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 Route::group([
-    //'middleware' => ['before' => 'jwt.auth']
+
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
 ], function ($router) {
-    Route::apiResource('users', UserController::class);
+
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+
 });
